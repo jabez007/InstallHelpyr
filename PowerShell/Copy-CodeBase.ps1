affc1506b1405377a0c6d7ed26bed1700e694c69
jabez007 <jabez.m.007@gmail.com>
2017-02-13 11:01:45 -0600

    Fixing some PowerShell

--- PowerShell/Copy-CodeBase.ps1
+++ PowerShell/Copy-CodeBase.ps1
@@ -3,6 +3,6 @@ Param( #Must be the first statement in your script
 	[string]$DestinationServer
 	)
 
-	Copy-Item -Path $SourceRoot\Release\Interconnect -Destination \\$DestinationServer\c$\Temp -recurse -force
+	Copy-Item -Path $SourceRoot\Release\Interconnect -Destination \\$DestinationServer\c$\Temp\Interconnect -recurse -force
 
     exit $LASTEXITCODE
